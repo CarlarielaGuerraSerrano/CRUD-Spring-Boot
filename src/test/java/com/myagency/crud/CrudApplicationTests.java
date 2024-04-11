7298b9a46d7e93c4d7918a58ecf4e490799b3be9
Carlariela <carlarielita@gmail.com>
2024-04-11 11:59:24 -0400

    creating crud git

---
+++ src/test/java/com/myagency/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myagency.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
